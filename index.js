831dd65f3d28be68c051dbe7b70ea4cbca824301
JaeHa <jaeha23@naver.com>
2021-01-29 16:15:18 +0900

    SSH, BodyParser, Postmas, 회원가입 기능

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+///application/json
+app.use(bodyParser.json());
 
 // mongoose 적용하기
 const mongoose = require("mongoose");
@@ -19,6 +27,19 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World! 제대로 하자");
+});
+
+// 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+// 그것들을 데이터베이스에 넣어준다.
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
